d6236603ea29028d0e60848d18f4b3eef9cefbdb
ahmed-alzahrani <ahmedalzahrani94@gmail.com>
2018-05-08 05:06:14 -0700

    initial commit

---
+++ FaceIt/FaceView.swift
@@ -0,0 +1,21 @@
+//
+//  FaceView.swift
+//  FaceIt
+//
+//  Created by Ahmed Al-Zahrani on 2018-05-04.
+//  Copyright © 2018 Ahmed Al-Zahrani. All rights reserved.
+//
+
+import UIKit
+
+class FaceView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ FaceIt/FacialExpression.swift
@@ -0,0 +1,9 @@
+//
+//  FacialExpression.swift
+//  FaceIt
+//
+//  Created by Ahmed Al-Zahrani on 2018-05-06.
+//  Copyright © 2018 Ahmed Al-Zahrani. All rights reserved.
+//
+
+import Foundation
